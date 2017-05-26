b21fe6ef718f1c4adc5518cd7425022639dff6e1
Nigel Horne <njh@bandsman.co.uk>
2017-05-26 12:30:28 -0400

    Release 0.03

--- lib/Geo/Coder/XYZ.pm
+++ lib/Geo/Coder/XYZ.pm
@@ -16,11 +16,11 @@ Geo::Coder::XYZ - Provides a geocoding functionality using https://geocode.xyz
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
